76852ae58537e4940a490e87ecb754521cbdafce
Anoob S A <109719075+anoobsa@users.noreply.github.com>
2022-10-18 10:49:12 +0530

    Update food.py

--- food.py
+++ food.py
@@ -6,7 +6,7 @@ class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("turtle")
+        self.shape("circle")
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.color("red")
         self.hideturtle()
